f27c304799115317e649bb64300434956c9a637b
sukhrobnuraliev <sukhrobnuralievv@gmail.com>
2022-09-15 00:17:36 +0500

    User model changed

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,7 +11,7 @@ use Laravel\Sanctum\HasApiTokens;
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
-
+    /*comment- delete me*/
     /**
      * The attributes that are mass assignable.
      *
