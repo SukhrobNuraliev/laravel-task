2754776a16447cec1de8c143671a4bfdb613f481
sukhrobnuraliev <sukhrobnuralievv@gmail.com>
2022-09-16 07:09:01 +0500

    creating only 1 application a day is done

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -6,6 +6,7 @@ use App\Jobs\SendEmailJob;
 use App\Mail\ApplicationCreated;
 use App\Models\Application;
 use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 
@@ -13,6 +14,10 @@ class ApplicationController extends Controller
 {
     public function store(Request $request)
     {
+        if ($this->checkDate()){
+            return redirect()->back()->with('error', 'You can create only 1 application a day');
+        }
+
         if ($request->hasFile('file')) {
             $name = $request->file('file')->getClientOriginalName();
             $path = $request->file('file')->storeAs(
@@ -38,5 +43,20 @@ class ApplicationController extends Controller
         dispatch(new SendEmailJob($application));
 
         return redirect()->back();
+    }
+
+    protected function checkDate()
+    {
+        if (auth()->user()->applications()->latest()->first() == null){
+            return false;
+        }
+
+        $last_application = auth()->user()->applications()->latest()->first();
+        $last_app_date = Carbon::parse($last_application->created_at)->format('Y-m-d');
+        $today = Carbon::now()->format('Y-m-d');
+
+        if ($last_app_date == $today){
+            return true;
+        }
     }
 }
--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Application;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
